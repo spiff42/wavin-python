4dcff63d32f2be52a1d45e6c719631a35334f5d7
Mikkel Holm Olsen <spiff@symlink.dk>
2019-06-26 16:40:37 +0200

    Created comms dump from OH and parsed it.

--- serdump.py
+++ serdump.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import serial
+import datetime
+import sys
 
 #ser = serial.Serial('/dev/tty-wavin-modbus', 38400, timeout=0.05)
 ser = serial.Serial('/dev/ttyUSB2', 38400, timeout=0.05)
@@ -9,6 +11,8 @@ while True:
 	st = ser.read(100)
 	if len(st) > 0:
 		hexstr = " ".join("{:02x}".format(ord(c)) for c in st)
-		print hexstr
+		dt = datetime.datetime.now()
+		print dt.strftime("%H:%M:%S") + (".%03d " % (dt.microsecond/1000)) + hexstr
+		sys.stdout.flush()
 
 
